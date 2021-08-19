455714fa3fe8137f802d9c5e88a96a01c595bddf
RomainoSoko <rom1.soko@gmail.com>
2021-08-19 21:07:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+import {validate} from 'schema-utils';
+
+const schema = {
+    type: 'object',
+    properties: {
+        test: {
+            type: 'string',
+        }
+    }
+}
+
+ export default class EnvRewritterPlugin {
+    static defaultOptions = {
+        files: ['./env', '.env.local'],
+    }
+
+    constructor(files = []) {
+        validate(schema, files, {
+            name: 'Env Rewritter Plugin',
+            baseDataPath: 'files',
+        });
+
+        this.files = [
+            ...new Set(
+                [ ...EnvRewritterPlugin.defaultOptions.files, ...this.files],
+            ),
+        ];
+    }
+
+    apply(compiler) {
+        const pluginName = EnvRewritterPlugin.name;
+
+        const { webpack } = compiler;
+
+        const { Compilation } = webpack;
+
+        const { RawSource } = webpack.sources;
+
+        compiler.hooks.environment.tap(pluginName, () => {
+           console.log('dddd', process.env);
+        });
+    }
+}
